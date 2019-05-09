c2ae82f075fbe6f9163e054525887c67483b14d1
Eliot Berriot <contact@eliotberriot.com>
2019-05-09 17:54:35 +0200

    Added env file and funkwhale user

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,38 +8,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-def test_hosts_file(host):
-    f = host.file("/etc/hosts")
-
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
-
-
-@pytest.mark.parametrize(
-    "package",
-    [
-        "python3",
-        "python3-dev",
-        "python3-pip",
-        "python3-venv",
-        "libldap2-dev",
-        "libsasl2-dev",
-        "git",
-        "unzip",
-        "build-essential",
-        "ffmpeg",
-        "libjpeg-dev",
-        "libmagic-dev",
-        "libpq-dev",
-        "postgresql-client",
-    ],
-)
-def test_installed_mandatory_packages(host, package):
-    package = host.package(package)
-    assert package.is_installed
-
-
 @pytest.mark.parametrize("service", ["redis-server", "postgresql", "nginx"])
 def test_installed_services(host, service):
     service = host.service(service)
@@ -62,3 +30,57 @@ def test_database_user_created(host):
     """
     result = host.run(cmd)
     assert result.stdout == "1"
+
+
+def test_funkwhale_user_creation(host):
+    user = host.user("funkwhale")
+    assert user.home == "/srv/funkwhale"
+    assert user.shell == "/bin/false"
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        "/srv/funkwhale/",
+        "/srv/funkwhale/data/media",
+        "/srv/funkwhale/data/static",
+        "/srv/funkwhale/data/music",
+    ],
+)
+def test_funkwhale_directories_creation(path, host):
+    dir = host.file(path)
+
+    assert dir.exists is True
+    assert dir.is_directory is True
+
+
+def test_funkwhale_env_file(host):
+    f = host.file("/srv/funkwhale/config/.env")
+
+    assert f.contains("MEDIA_ROOT=/srv/funkwhale/data/media") is True
+    assert f.contains("STATIC_ROOT=/srv/funkwhale/data/static") is True
+    assert f.contains("MUSIC_DIRECTORY_PATH=/srv/funkwhale/data/music") is True
+    assert f.contains("MUSIC_DIRECTORY_SERVE_PATH=/srv/funkwhale/data/music") is True
+    assert f.contains("FUNKWHALE_HOSTNAME=yourdomain.funkwhale") is True
+    assert f.contains("FUNKWHALE_PROTOCOL=https") is True
+    assert f.contains("DJANGO_SECRET_KEY=") is True
+    assert f.contains("FUNKWHALE_API_IP=127.0.0.1") is True
+    assert f.contains("FUNKWHALE_API_PORT=5000") is True
+    assert f.contains("REVERSE_PROXY_TYPE=nginx") is True
+    assert f.contains("DATABASE_URL=postgresql://funkwhale@:5432/funkwhale") is True
+    assert f.contains("CACHE_URL=redis://127.0.0.1:6379/0") is True
+    assert (
+        f.contains("EMAIL_CONFIG=smtp+tls://user@:password@youremail.host:587") is True
+    )
+    assert f.contains("DEFAULT_FROM_EMAIL=noreply@yourdomain") is True
+    assert f.contains("FUNKWHALE_FRONTEND_PATH=/srv/funkwhale/frontend/dist") is True
+    assert (
+        f.contains("FUNKWHALE_SPA_HTML_ROOT=/srv/funkwhale/frontend/dist/index.html")
+        is True
+    )
+    assert f.contains("NGINX_MAX_BODY_SIZE=100M") is True
+    assert f.contains("DJANGO_SETTINGS_MODULE=config.settings.production") is True
+
+    # additional vars
+    assert f.contains("ADDITIONAL_VAR=1") is True
+    assert f.contains("ADDITIONAL_VAR=2") is True
